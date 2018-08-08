32cc1c87abec42e640fedef7e2682b70d2df8071
Josemi213 <jm.rojassanchez@ugto.mx>
2018-08-08 09:20:22 -0500

    semana1

---
+++ semana1/primer_programa.c
@@ -0,0 +1,10 @@
+/*Creadoel 08 de Agosto del 2018 por Misael "Josemi213" Rojas*/
+#include<stdio.h>
+int main()
+{
+	printf("¡¿Quien es ese pokemon?! \n");
+	 printf("Este es el primer programa del curso\n");
+}
+
+	/*Termine de escribir el programa*/
+
