14e837b167dc100945c2d7300093735d2ab1cf6b
Josemi213 <jm.rojassanchez@ugto.mx>
2018-08-08 08:54:22 -0500

    Este documento ha sido actualizado, los cambios han sido hechos.

--- semana2/programa_hola.c
+++
@@ -1,5 +0,0 @@
-/*
-Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por ejemplo: Este programa imprime la típica frase "hola mundo a la pantalla"
-Creado el 06/08/2018 por Misael "Josemi213" Rojas.
-*/
-
